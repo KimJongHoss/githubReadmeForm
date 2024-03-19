966285be1b52a19de2b1bf0ee06f6e989de702b4
cloud <goorm94@naver.com>
2024-03-19 15:27:14 +0900

    first commit

---
+++ src/main/java/hg/jh/luko4/Luko4Application.java
@@ -0,0 +1,13 @@
+package hg.jh.luko4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Luko4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Luko4Application.class, args);
+    }
+
+}
---
+++ src/test/java/hg/jh/luko4/Luko4ApplicationTests.java
@@ -0,0 +1,13 @@
+package hg.jh.luko4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Luko4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
